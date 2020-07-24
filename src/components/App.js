e68d5b88af556f2f7de4e6db5a3ded38f4b30d7b
Michael Kane <kanemike7@gmail.com>
2020-07-24 13:12:41 -0500

    imported MovieList component and added component with movie data

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import MovieList from './MovieList';
+
 
 class App extends React.Component {
-  render(){
-    return(
-    <div>
-      Starter App component - hello there
-    </div>
-  )}
+  render() {
+    return (
+      <div>
+        Starter App component - hello there
+        <MovieList movies={[
+          { title: 'Mean Girls' },
+          { title: 'Hackers' },
+          { title: 'The Grey' },
+          { title: 'Sunshine' },
+          { title: 'Ex Machina' },
+        ]} />
+      </div>
+    )
+  }
 }
 
 export default App;
