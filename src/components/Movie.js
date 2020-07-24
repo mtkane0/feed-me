50ea993e93c72d3d573ef393c5d79b2fbb205bf3
Michael Kane <kanemike7@gmail.com>
2020-07-24 17:00:51 -0500

    Add state, hover and click handlers

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,44 @@
 import React from 'react';
 
 
-let Movie = (props) => {
-  console.log('movie props', props)
-  return (
-    <div style={{ padding: '20px', borderStyle: 'solid' }}>{props.title}</div>
-  )
+class Movie extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hover: false
+    };
+
+    this.handleOnClick = this.handleOnClick.bind(this);
+    this.handleOnMouseOver = this.handleOnMouseOver.bind(this);
+  }
+
+
+
+  handleOnClick(e) {
+    console.log('clicked', this.props.title);
+  }
+
+  handleOnMouseOver(e) {
+    // console.log('hovering...', this.props.title);
+    // console.log('state.hover: ', this.state.hover);
+    this.setState({
+      hover: !this.state.hover
+    })
+  }
+
+  render() {
+    // console.log('Movie props', this.props);
+    let style = {
+      padding: '20px',
+      borderStyle: 'solid',
+      backgroundColor: this.state.hover ? 'green' : 'transparent'
+    };
+
+    return (
+      <div style={style} onClick={this.handleOnClick} onMouseEnter={this.handleOnMouseOver} onMouseLeave={this.handleOnMouseOver}>{this.props.title}</div>
+    )
+  }
 }
 
 export default Movie;
