9fe030870c0000f2bfee4125c718d8c8bddf30ff
Michael Kane <kanemike7@gmail.com>
2020-07-25 11:20:54 -0500

    Search will find matches, but does not reset when the user deletes entry

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,42 +1,67 @@
 import React from 'react';
-import Movie from './Movie';
-import movies from '../index.js';
+import MovieList from './MovieList';
+import App from './App';
 
 class Search extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      textEntered: ''
+      textEntered: '',
+      movies: [
+        { title: 'Mean Girls' },
+        { title: 'Hackers' },
+        { title: 'The Grey' },
+        { title: 'Sunshine' },
+        { title: 'Ex Machina' },
+      ]
     }
 
-    this.handleOnEnterSearch = this.handleOnEnterSearch.bind(this);
-    this.handleOnGo = this.handleOnGo.bind(this);
+    this.handleTextEntered = this.handleTextEntered.bind(this);
+    this.handleMovieSearch = this.handleMovieSearch.bind(this);
   }
 
-  handleOnEnterSearch(e) {
-    console.log('entered search bar')
+  handleTextEntered(e) {
     this.setState({
       textEntered: e.target.value
+    });
+    this.handleMovieSearch(this.state.textEntered);
+    // console.log('movies:', this.state.movies);
+  }
+
+  handleMovieSearch(text) {
+    console.log('RUNNING HANDLE ON GO')
+    this.setState({
+      movies: this.state.movies.filter((movie) => {
+        console.log('text', text, movie.title.includes(text));
+        return movie.title.includes(text);
+      })
     })
   }
 
-  handleOnGo(e) {
-    console.log('user entered', this.state.textEntered);
-  }
-
   render() {
+    console.log('entered search bar', this.state.textEntered);
 
     let divStyle = {
       padding: '20px',
       textAlign: 'center',
     };
 
+    let sorry = <div style={divStyle}><p>Sorry, no matches</p></div>;
     return (
       <div>
         <div style={divStyle}>
-          <input onChange={this.handleOnEnterSearch} type="text" name="search" placeholder="Search..." />
-          <input onClick={this.handleOnGo} type="submit" value="Go!" />
+          <input onChange={this.handleTextEntered} type="text" name="search" placeholder="Search..." />
+          <input type="submit" value="Go!" />
+        </div>
+        <div>
+          {
+            this.state.movies.length
+              ?
+              <MovieList movies={this.state.movies} />
+              :
+              sorry
+          }
         </div>
       </div>
     )
