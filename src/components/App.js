e964380376b6028193254dd6d92b33441c3934ac
Michael Kane <kanemike7@gmail.com>
2020-07-25 16:06:19 -0500

    Implement adding to movies list

--- src/components/App.js
+++ src/components/App.js
@@ -8,8 +8,26 @@ class App extends React.Component {
     super(props)
 
     this.state = {
-
+      addMovie: ''
     }
+    this.handleAddMovie = this.handleAddMovie.bind(this);
+    this.handleText = this.handleText.bind(this);
+  }
+
+  handleText(e) {
+    this.setState({
+      addMovie: e.target.value
+    })
+  }
+
+  handleAddMovie(e) {
+    //console.log('Add ', movieTitle);
+    let movie = this.state.addMovie;
+    this.setState({
+      addMovie: ''
+    })
+    console.log('this.props', this.props)
+    this.props.movies.push({ title: movie })
   }
 
   render() {
@@ -29,6 +47,10 @@ class App extends React.Component {
           <h2>MovieList</h2>
         </div>
         <div style={movieListStyle}>
+          <div>
+            <input type="text" name="addMovie" onChange={this.handleText} value={this.state.addMovie} />
+            <input type="button" value="Add" onClick={this.handleAddMovie} />
+          </div>
           <div>
             <Search movies={this.props.movies} />
           </div>
