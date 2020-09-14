fa0212463f63bbdc2bafdce96bc831bc3c629ccd
Michael Kane <kanemike7@gmail.com>
2020-09-14 17:25:17 -0500

    Clean repp

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-let movies = require('./components/data/movies');
 
-
-ReactDOM.render(<App movies={movies.movies} />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
