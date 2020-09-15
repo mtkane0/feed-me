12c61b75d9a0de9b123987be4c636b5f52c54677
Michael Kane <kanemike7@gmail.com>
2020-09-14 23:43:24 -0500

    Handle route that gets lat and lng, then gets nearby locations and sends back to client

--- server/index.js
+++ server/index.js
@@ -1,12 +1,49 @@
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
+const KEY = require('../config.js');
 const app = express();
 const PORT = 3030;
 
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.use('/', express.static(path.join(__dirname, '../public')))
 
-app.get('/', (req, res) => {
-  res.send('hell from server');
+app.get('/searchBy/:zipcode', (req, res) => {
+  let zip = req.params.zipcode;
+  axios({
+    method: 'get',
+    url: 'https://maps.googleapis.com/maps/api/geocode/json',
+    params: {
+      key: KEY.GMP_API_KEY,
+      address: zip,
+    }
+  })
+    .then((location) => {
+      let latlong = location.data.results[0].geometry.location.lat + ',' + location.data.results[0].geometry.location.lng;
+      console.log('latlong', latlong);
+      axios({
+        method: 'get',
+        url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json',
+        params: {
+          key: KEY.GMP_API_KEY,
+          location: latlong,
+          keyword: 'restaurant',
+          radius: 10000,
+        }
+      })
+        .then((places) => {
+          res.send(places.data);
+        })
+        .catch((err) => {
+          res.status(403).send(err);
+        })
+    })
+    .catch((err) => {
+      console.log('Err getting location', err);
+      res.status(403).send('Invalid Request');
+    })
 });
 
 app.listen(PORT, () => {
