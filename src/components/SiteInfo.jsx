bce6206189e5a3fca404a71aaa0d2f41d1fcff4d
Michael Kane <kanemike7@gmail.com>
2020-09-14 23:44:50 -0500

    Add styling to text

--- src/components/SiteInfo.jsx
+++ src/components/SiteInfo.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
 
 const SiteInfo = () => {
+  const infoStyle = {
+    padding: '10%',
+    fontFamily: '-apple-system',
+  }
   return (
     <div>
-      <p>Site description/instructions</p>
+      <Typography style={infoStyle}
+        align="center"
+        variant="h6"
+      >Hungry? Enter your zip code and we will find places near you.</Typography>
     </div>
   )
 };
