3810308b1122863b0a69bedf1df64c96c9c89dda
Michael Kane <kanemike7@gmail.com>
2020-09-14 23:46:02 -0500

    Implement event handlers for text change and button click

--- src/components/ZipForm.jsx
+++ src/components/ZipForm.jsx
@@ -1,46 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
 
 
-const ZipForm = () => {
+class ZipForm extends React.Component {
 
-  const searchStyle = {
-    width: '100%',
-    fontSize: '1.5rem',
-    marginLeft: 'auto',
-    marginRight: 'auto',
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      zip: ''
+    }
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
-  const buttonStyle = {
-    backgroundColor: '#ffb766',
-    fontSize: '1em',
-    width: '100%'
+  handleInputChange(e) {
+    this.setState({
+      zip: e.target.value
+    })
   }
 
-  // const innerBoxStyle = {
-  //   display: 'inline',
-  //   width: '50%',
+  render() {
+    const searchStyle = {
+      width: '100%',
+      fontSize: '1.5rem',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    }
 
-  //   border: 'solid red 1px'
-  // }
-
-  return (
-    <div >
-      <form>
-        <Box style={{ width: '40vw' }}>
-          <TextField style={searchStyle}
-            label="Zip Code"
-            variant="outlined"
-          />
-          <Button style={buttonStyle}
-            variant="contained"
-          >Find Food!</Button>
-        </Box>
-      </form>
-    </div >
-  )
+    const buttonStyle = {
+      backgroundColor: '#ffb766',
+      fontSize: '1em',
+      width: '100%'
+    }
+    return (
+      <div>
+        <form>
+          <Box style={{ width: '40vw' }}>
+            <TextField style={searchStyle}
+              placeholder="Ex. 94043"
+              label="Zip Code"
+              variant="outlined"
+              value={this.state.zip}
+              onChange={this.handleInputChange}
+            />
+            <Button style={buttonStyle}
+              variant="contained"
+              onClick={() => this.props.handleSubmit(this.state.zip)}
+            >Find Food!</Button>
+          </Box>
+        </form>
+      </div >
+    )
+  }
 };
 
 export default ZipForm;
