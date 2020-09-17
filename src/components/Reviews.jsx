ba1e85f54765c574da6ddaa4c22d211cfe5f192a
Michael Kane <kanemike7@gmail.com>
2020-09-17 11:27:36 -0500

    Use to display reviews in icons

---
+++ src/components/Reviews.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import FastfoodIcon from '@material-ui/icons/Fastfood';
+import Rating from '@material-ui/lab/Rating';
+
+
+
+const Reviews = (props) => {
+  return (
+    <FastfoodIcon />
+  )
+};
+
+export default Reviews;
